f4e3f473a62513d78800405ff5f957c779bcc285
ynitylearn <ynitylearn@gmail.com>
2025-07-26 23:34:00 +0100

    second commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,47 +1,31 @@
-import { useState } from 'react';
+// Sidebar.jsx
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
-import { FaHome, FaListAlt, FaCog, FaSignOutAlt, FaBars, FaTimes } from 'react-icons/fa';
+import { FaHome, FaListAlt, FaCog, FaSignOutAlt } from 'react-icons/fa';
 import logo from "../assets/logo_to_do_sans_bg.png";
 
+
 export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <>
-      <button className="menu-toggle" onClick={toggleSidebar}>
-        {isOpen ? <FaTimes /> : <FaBars />}
-      </button>
-
-      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-        <div className="sidebar-header">
-          <div className="header-content">
-            <img src={logo} alt="Logo" className="logoNavbar" />
-            <span>To-Do App</span>
-          </div>
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <div className="header-content">
+          <img src={logo} alt="Logo" className="logoNavbar" />
+          <span>To-Do App</span>
         </div>
-        <nav className="sidebar-nav">
-          <NavLink 
-            to="/todos" 
-            className={({ isActive }) => isActive ? 'sidebar-link active' : 'sidebar-link'}
-            onClick={() => window.innerWidth < 992 && setIsOpen(false)}
-          >
-            <FaListAlt /> <span>Mes Tâches</span>
-          </NavLink>
-
-          <NavLink 
-            to="/logout" 
-            className={({ isActive }) => isActive ? 'sidebar-link active' : 'sidebar-link'}
-            onClick={() => window.innerWidth < 992 && setIsOpen(false)}
-          >
-            <FaSignOutAlt /> <span>Déconnexion</span>
-          </NavLink>
-        </nav>
       </div>
-    </>
+      <nav className="sidebar-nav">
+
+
+        <NavLink to="/todos" className="sidebar-link">
+          <FaListAlt /> <span>My Tâches</span>
+        </NavLink>
+
+
+        <NavLink to="/logout" className="sidebar-link">
+          <FaSignOutAlt /> <span>Déconnexion</span>
+        </NavLink>
+      </nav>
+    </div>
   );
 }
